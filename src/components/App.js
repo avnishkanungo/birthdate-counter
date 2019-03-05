0e672f77b6853d9fd13afdf60d35695d85506963
Avnish Kanungo <avnishkanungo@Avnishs-MacBook-Pro.local>
2019-03-05 09:41:23 +0530

    fade in annimation added

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,10 @@ class App extends Component{
                     </Button>
                     {
                         this.state.showStats ?
-                            <AgeStats date={this.state.birthday}/>
+                            <div className='fade age-stats'>
+                                <AgeStats date={this.state.birthday}/>
+                            </div>
+                            
                         :
                             <div></div>
                     }
